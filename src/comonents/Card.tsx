7dc3add18bd8dd0b105762c82c167d03333fa69e
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-04-25 13:45:53 +0100

    added fonts

--- src/comonents/Card.tsx
+++ src/comonents/Card.tsx
@@ -1,18 +1,18 @@
 export default function Card(){
     return(
         <>
-        <div className="bg-white w-fit mx-auto my-32">
+        <div className="bg-white w-fit mx-auto my-32 font-['Red_Hat_Display']">
         <img src="src/assets/illustration-hero.svg" alt="" />
-            <div className="w-96 mx-auto">
-                <h1>Order Summary</h1>
-                <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+            <div className="w-96 mx-auto ">
+                <h1 className="">Order Summary</h1>
+                <p className="">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
                 <div className="">
-                    <h2>Annual Plan</h2>
-                    <h4>$59.99/year</h4>
-                    <button>Change</button>
+                    <h2 className="">Annual Plan</h2>
+                    <h4 className="">$59.99/year</h4>
+                    <button className="">Change</button>
                 </div>
-                <button>Proceed to Payment</button>
-                <button>Cancel Order</button>
+                <button className="">Proceed to Payment</button>
+                <button className="">Cancel Order</button>
             </div>
         </div>
         </>
