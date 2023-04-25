02e36f81128dc6d404208c3c16da2772c3938139
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-04-25 13:19:27 +0100

    added background and html for card component

---
+++ src/comonents/Card.tsx
@@ -0,0 +1,17 @@
+export default function Card(){
+    return(
+        <>
+            <div className="">
+                <h1>Order Summary</h1>
+                <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+                <div className="">
+                    <h2>Annual Plan</h2>
+                    <h4>$59.99/year</h4>
+                    <button>Change</button>
+                </div>
+                <button>Proceed to Payment</button>
+                <button>Cancel Order</button>
+            </div>
+        </>
+    )
+}
