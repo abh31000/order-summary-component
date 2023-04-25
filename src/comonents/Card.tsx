f3c35562aeaf8e8064ecfe79df7c56a53c0b65f6
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-04-25 15:09:20 +0100

    fixing images

--- src/comonents/Card.tsx
+++ src/comonents/Card.tsx
@@ -2,13 +2,13 @@ export default function Card(){
     return(
         <>
         <div className="bg-white rounded-xl overflow-hidden drop-shadow-lg w-fit mx-auto my-7 font-['Red_Hat_Display']">
-        <img className="w-fit" src="src/assets/illustration-hero.svg" alt="" />
+        <img className="w-fit" src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682428063/illustration-hero_wxutdz.svg" alt="" />
             <div className="w-96 flex-col space-y-3 pt-8 p-6 mx-auto ">
                 <h1 className="text-2xl text-center text-[#1f2f56] font-extrabold">Order Summary</h1>
                 <p className="text-base text-center text-[#7280a7]">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
                 <div className="flex justify-between pt-4 px-5">
                     <div className="flex justify-center space-x-4">
-                        <img src="src/assets/icon-music.svg" alt="" />
+                        <img src="https://res.cloudinary.com/dbiudcrba/image/upload/v1682428063/icon-music_jewbyp.svg" alt="" />
                         <div className="">
                             <h2 className="font-extrabold text-[#1f2f56]">Annual Plan</h2>
                             <h4 className="text-[#7280a7]">$59.99/year</h4>
